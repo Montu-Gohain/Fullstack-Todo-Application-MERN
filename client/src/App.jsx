492041f324ba2bca602e676652642248eb5c1ceb
Montu-Gohain <leomontugohain@gmail.com>
2023-02-04 16:20:16 +0530

    Changing the title of navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,31 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import ProtectedRoute from "./components/ProtectedRoute"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
-export default function App () {
-    return (
-        <div className="wrapper bg-dark text-white">
-            <Navbar title="Mern Todos"/>
-            <div className="container">
-                <Routes>
-                    <Route path="/" element={
-                        <ProtectedRoute>
-                            <Home/>
-                        </ProtectedRoute>
-                    }/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path='/*' element={<Navigate to="/"/>}/>
-                </Routes>
-            </div>
-            <Footer/>
-        </div>
-    ) 
+export default function App() {
+  return (
+    <div className='wrapper bg-dark text-white'>
+      <Navbar title='List of Todos' />
+      <div className='container'>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/*' element={<Navigate to='/' />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 }
